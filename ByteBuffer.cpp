b0ab218f8594a2a4625fb0ddcd6f7d501f3b60f3
Runescape-Reversed <80010206+Runescape-Reversed@users.noreply.github.com>
2021-03-03 17:46:51 -0500

    Update ByteBuffer.cpp

--- ByteBuffer.cpp
+++ ByteBuffer.cpp
@@ -1,12 +1,14 @@
-void __fastcall ByteBuffer::WriteShortNeg(INT16 Short)
-{
-  UINT64 off1; // r9
-  UINT64 off2; // r8
+#include "ByteBuffer.h"
 
-  off1 = CurrentOffset;
+VOID ByteBuffer::WriteShortNeg(
+  _In_ INT16 Short
+)
+{
+  UINT64 off1 = CurrentOffset;
   CurrentOffset = off1 + 1;
   Buffer[off1] = Short + -128;
-  off2 = CurrentOffset;
+
+  UINT64 off2 = CurrentOffset;
   CurrentOffset = off2 + 1;
   Buffer[off2] = HIBYTE(Short);
 }
